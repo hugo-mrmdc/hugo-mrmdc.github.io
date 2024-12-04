03f4cf48c5cc0f8dd40d890173a911acdff7e936
hugo-mrmdc <hugo.mermoudclairbaux@gmail.com>
2024-12-04 15:03:23 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "pokemon-tracker-cache-v1";
+const urlsToCache = [
+    "./index.html",
+    "./manifest.json",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
+    "https://unpkg.com/leaflet/dist/leaflet.css",
+    "https://unpkg.com/leaflet/dist/leaflet.js",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
